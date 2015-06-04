57495a42cea81b3ca013bd45d4e1ac8d3d13e95f
mikey <michaelrayvon@gmail.com>
2015-06-03 19:03:30 -0500

    fix jscs issues

--- app/collections/items.js
+++ app/collections/items.js
@@ -14,26 +14,30 @@ Items.allow({
   insert: function(userId, doc){
     return can.createItem(userId);
   },
-  update:  function(userId, doc, fieldNames, modifier){
+
+  update: function(userId, doc, fieldNames, modifier){
     return can.editItem(userId, doc);
   },
-  remove:  function(userId, doc){
+
+  remove: function(userId, doc){
     return can.removeItem(userId, doc);
-  }
+  },
 });
 
 // Methods
 
 Meteor.methods({
   createItem: function(item){
-    if(can.createItem(Meteor.user()))
+    if(can.createItem(Meteor.user())){
       Items.insert(item);
+    }
   },
+
   removeItem: function(item){
     if(can.removeItem(Meteor.user(), item)){
       Items.remove(item._id);
     }else{
-      throw new Meteor.Error(403, 'You do not have the rights to delete this item.')
+      throw new Meteor.Error(403, 'You do not have the rights to delete this item.');
     }
-  }
+  },
 });
