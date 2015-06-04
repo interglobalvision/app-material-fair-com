6a39a1b09a06e531d1c11a968a3c3aa57d203d25
mikey <michaelrayvon@gmail.com>
2015-06-03 19:31:45 -0500

    add bare exhibitor templates

---
+++ app/client/templates/exhibitor/events.js
@@ -0,0 +1,15 @@
+Template.events.created = function () {
+  //
+};
+
+Template.events.helpers({
+  //
+});
+
+Template.events.rendered = function () {
+  //
+};
+
+Template.events.events({
+  //
+});
