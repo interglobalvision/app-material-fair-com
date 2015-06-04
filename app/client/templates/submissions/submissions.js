b6c17eca6e7e3073208c26e1641a3e0b78933b02
mikey <michaelrayvon@gmail.com>
2015-06-03 19:34:20 -0500

    add bare submissions templates

---
+++ app/client/templates/submissions/submissions.js
@@ -0,0 +1,15 @@
+Template.submissions.created = function () {
+  //
+};
+
+Template.submissions.helpers({
+  //
+});
+
+Template.submissions.rendered = function () {
+  //
+};
+
+Template.submissions.events({
+  //
+});
