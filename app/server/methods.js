45acc3812ac9df332f380331b332c6dcf64ef286
mikey <michaelrayvon@gmail.com>
2015-06-05 17:42:05 -0500

    clean up lint

--- app/server/methods.js
+++ app/server/methods.js
@@ -3,10 +3,10 @@ Meteor.methods({
   onCreateUserAddRole: function(userId) {
     if (Meteor.users.find().count() === 1) {
       console.log('super');
-      Roles.addUsersToRoles(userId, ['super']);
+      Roles.addUsersToRoles(userId, ['super',]);
     } else {
       console.log('applicant');
-      Roles.addUsersToRoles(userId, ['applicant']);
+      Roles.addUsersToRoles(userId, ['applicant',]);
     }
   },
-});
+}); 
