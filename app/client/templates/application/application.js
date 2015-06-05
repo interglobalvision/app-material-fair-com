9078fe7dc14d0504d8547af6c491d92b8eb1b9d5
cas <casska@gmail.com>
2015-06-05 18:10:44 -0500

    moves schemas to its own file add save form button and sets event function

--- app/client/templates/application/application.js
+++ app/client/templates/application/application.js
@@ -1,9 +1,9 @@
 Template.application.created = function () {
-  //
 };
 
 Template.application.helpers({
-  //
+  applicationDocId: function() {
+  },
 });
 
 Template.application.rendered = function () {
@@ -11,5 +11,15 @@ Template.application.rendered = function () {
 };
 
 Template.application.events({
-  //
+  'click input[value="Save"]': function(e) {
+    e.preventDefault();
+
+    // Get form values
+    var applicationValues = AutoForm.getFormValues('insertApplicationForm');
+    
+    console.log(applicationValues);
+    
+    // Update values
+  },
 });
+
---
+++ app/lib/schemas.js
@@ -0,0 +1,181 @@
+Schemas = {};
+
+// Application Form Schema
+Schemas.applicationForm = new SimpleSchema({
+  submitted: {
+    type: Boolean,
+    label: 'Submitted',
+    defaultValue: false,
+  },
+  signed: {
+    type: Boolean,
+    label: 'Signed',
+    defaultValue: false,
+  },
+  paid: {
+    type: Boolean,
+    label: 'Paid',
+    defaultValue: false,
+  },
+  approved: {
+    type: Boolean,
+    label: 'Approved',
+    defaultValue: false,
+  },
+  rating: {
+    type: Number,
+    label: 'Rating',
+    defaultValue: 0,
+  },
+
+  // General Information
+  galleryName: {
+    type: String,
+    label: 'Gallery Name',
+  },
+
+  // -- Address
+  address: {
+    type: Object,
+    label: 'Address',
+    optional: true,
+  },
+  'address.line1': {
+    type: String,
+    label: '',
+  },
+  'address.line2': {
+    type: String,
+    label: '',
+  },
+  'address.city': {
+    type: String,
+    label: 'City',
+  },
+  'address.state': {
+    type: String,
+    label: 'State (if applicable)',
+    optional: true,
+  },
+  'address.postalCode': {
+    type: String,
+    label: 'Postal Code (if applicable)',
+    optional: true,
+  },
+  galleryYear: {
+    type: Number,
+    label: 'Year gallery was founded',
+  },
+  participation: {
+    type: String,
+    label: 'Participation in other fairs',
+    optional: true,
+    max: 2000,
+    autoform: {
+      rows: 10,
+    },
+  },
+  website: {
+    type: String,
+    label: 'Website',
+    regEx: SimpleSchema.RegEx.Url,
+    optional: true,
+  },
+  twitter: {
+    type: String,
+    label: 'Twitter Username',
+    optional: true,
+  },
+  facebook: {
+    type: String,
+    label: 'Facebook',
+    optional: true,
+  },
+  tumblr: {
+    type: String,
+    label: 'Tumblr',
+    optional: true,
+  },
+  instagram: {
+    type: String,
+    label: 'Instagram',
+    optional: true,
+  },
+
+  // Primary Contact
+  'contact.name': {
+    type: String,
+    label: 'Name',
+  },
+  'contact.email': {
+    type: String,
+    label: 'Email',
+    regEx: SimpleSchema.RegEx.Email,
+  },
+  'contact.phone': {
+    type: String,
+    label: 'Mobile Phone',
+  },
+  galleryHistory: {
+    type: String,
+    label: 'Brief history of the gallery',
+    max: 2000,
+    autoform: {
+      rows: 10,
+    },
+  },
+
+  // Proposal
+  standProposal: {
+    type: String,
+    label: 'Proposal for Material Art Fair 2016 stand',
+    max: 2000,
+    autoform: {
+      rows: 10,
+    },
+  },
+  artists: {
+    type: [Object,],
+    label: 'Artists represented by the gallery',
+    min: 1,
+  },
+  /*
+   * Gotta fogure it out how to save and relate pdf, i think with FS u just save the ID
+   *
+  'proposal.artists.$.cv': {
+    type: String,
+    label: 'Artist name',
+  },
+  */
+  'artists.$.name': {
+    type: String,
+    label: 'Artist name',
+  },
+  'artists.$.workTitle': {
+    type: String,
+    label: 'Work title',
+  },
+  'artists.$.medium': {
+    type: String,
+    label: 'Medium',
+  },
+  'artists.$.dimensions': {
+    type: String,
+    label: 'Dimensions',
+  },
+  'artists.$.year': {
+    type: Number,
+    label: 'Year',
+  },
+  boothType: {
+    type: String,
+    label: "Booth size",
+    allowedValues: [
+      'Project',
+      'Small',
+      'Medium',
+      'Medium Plus',
+      'Large',
+    ],
+  },
+});
