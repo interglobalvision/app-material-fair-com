2baa1178bd65ab3f2a1c7f4d39c8d52fd5543dd1
mikey <michaelrayvon@gmail.com>
2015-06-16 16:54:29 -0500

    add mobile nav

--- app/client/templates/common/header.js
+++ app/client/templates/common/header.js
@@ -1,3 +1,9 @@
+Template.header.rendered = function () {
+	$(".button-collapse").sideNav({
+		closeOnClick: true
+	});
+};
+
 Template.header.helpers({
   isLoggedIn: function () {
     return !!Meteor.user();
