9ca677d928e3995650d7fa732928c7e33506b01a
mikey <michaelrayvon@gmail.com>
2015-06-16 17:22:35 -0500

    add setLanguage to language switch

--- app/client/templates/common/header.js
+++ app/client/templates/common/header.js
@@ -2,6 +2,13 @@ Template.header.rendered = function () {
 	$(".button-collapse").sideNav({
 		closeOnClick: true
 	});
+	$('#lang-lever').click(function() {
+		if($('#lang').is(':checked')) {
+			TAPi18n.setLanguage('en');
+		} else {
+			TAPi18n.setLanguage('es');
+		}
+	})
 };
 
 Template.header.helpers({
