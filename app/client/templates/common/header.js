ed17601928b8e28a75ce88cb7f89b24d906e9c0a
mikey <michaelrayvon@gmail.com>
2015-06-16 17:40:07 -0500

    lint js

--- app/client/templates/common/header.js
+++ app/client/templates/common/header.js
@@ -1,14 +1,14 @@
 Template.header.rendered = function () {
 	$(".button-collapse").sideNav({
-		closeOnClick: true
+		closeOnClick: true,
 	});
 	$('#lang-lever').click(function() {
-		if($('#lang').is(':checked')) {
+		if ($('#lang').is(':checked')) {
 			TAPi18n.setLanguage('en');
 		} else {
 			TAPi18n.setLanguage('es');
 		}
-	})
+	});
 };
 
 Template.header.helpers({
