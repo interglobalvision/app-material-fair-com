fbc7aa78993895fb36ded0e83bf7e75cc1610f3e
mikey <michaelrayvon@gmail.com>
2015-06-17 19:04:26 -0500

    clean up indents

--- app/server/emailTemplates.js
+++ app/server/emailTemplates.js
@@ -2,9 +2,9 @@
 Accounts.emailTemplates.siteName = "Material Art Fair";
 Accounts.emailTemplates.from = "Material Art Fair Admin <accounts@example.com>";
 Accounts.emailTemplates.enrollAccount.subject = function (user) {
-    return "Your Material Art Fair " + user.roles[0] + " account.";
+	return "Your Material Art Fair " + user.roles[0] + " account.";
 };
 
 Accounts.emailTemplates.enrollAccount.text = function (user, url) {
-   return "You have a new account on the Material Art Fair application & review." + " To activate your account, simply click the link below:\n\n" + url;
+	return "You have a new account on the Material Art Fair application & review." + " To activate your account, simply click the link below:\n\n" + url;
 };
