2c8ca6f244894fbfbc3d574fe59e73ee72f7f964
mikey <michaelrayvon@gmail.com>
2015-06-21 16:56:40 -0500

    remove paypal stuff

--- app/client/templates/application/signed.js
+++ app/client/templates/application/signed.js
@@ -12,20 +12,10 @@ Template.signed.rendered = function () {
   $('select').material_select();
 };
 
-Template.paypalCreditCardForm.events({
-  'submit #paypal-payment-form': function(event, tmp){
-    event.preventDefault();
-
-    var cardData = Template.paypalCreditCardForm.card_data();
+Template.signed.events({
+  'submit #payment-form': function(e){
+    e.preventDefault();
 
     $('button[type="submit"]').attr('disabled','disabled');
-/*
-    Meteor.Paypal.purchase(cardData, {total: '100.50', currency: 'USD'}, function(error, results){
-      if (error) {
-      	Materialize.toast(error.reason, 3000);
-    	} else {
-    		Materialize.toast('Ok! Payment recieved.', 3000);
-    	}
-    });*/
   },
 });
--- app/server/paypal.js
+++
@@ -1,6 +0,0 @@
-Meteor.Paypal.config({
-  'host': 'api.sandbox.paypal.com',
-  'port': '',
-  'client_id': 'Your Paypal Client Id',
-  'client_secret': 'Your Paypal Client Secret'
-});
