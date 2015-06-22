2e9f8a85de6fd12e5b05c00e5019482a2a8b627c
mikey <michaelrayvon@gmail.com>
2015-06-22 18:50:08 -0500

    update payment form

--- app/client/templates/application/signed.js
+++ app/client/templates/application/signed.js
@@ -13,37 +13,39 @@ Template.signed.rendered = function () {
 };
 
 Template.signed.events({
-  'click #submit-payment': function(e){
-    e.preventDefault();
+	'click #submit-payment': function(e){
+		e.preventDefault();
 
-  // Disable submit button
-    //$('input#submit-payment').attr('disabled','disabled');
+		// Disable submit button
+		//$('input#submit-payment').attr('disabled','disabled');
 
-    var first = $('#first-name').val(),
-  	last = $('#last-name').val(),
-  	cardNumber = $('#card-number').val().replace(/\D/g,''),
-  	cvt = $('#cvt').val().replace(/\D/g,''),
-  	postalCode = $('#postal-code').val().replace(/\D/g,''),
-  	month = $('#exp-month').val().replace(/\D/g,''),
-  	year = $('#exp-year').val().replace(/\D/g,''),
-  	amount = $('#amount').val(),
-  	email = $('#email').val(),
-  	phone = $('#phone').val(),
-  	cell = $('#cell').val(),
-  	address1 = $('#address-1').val(),
-  	address2 = $('#address-2').val(),
-  	city = $('#city').val(),
-  	state = $('#state').val(),
-  	country = $('#country').val();
+		var first = $('#first-name').val(),
+		last = $('#last-name').val(),
+		cardNumber = $('#card-number').val().replace(/\D/g,''),
+		cvc = $('#cvc').val().replace(/\D/g,''),
+		postalCode = $('#postal-code').val().replace(/\D/g,''),
+		month = $('#exp-month').val().replace(/\D/g,''),
+		year = $('#exp-year').val().replace(/\D/g,''),
+		amount = $('#amount').val(),
+		email = $('#email').val(),
+		phone = $('#phone').val().replace(/\D/g,''),
+		cell = $('#cell').val().replace(/\D/g,''),
+		address1 = $('#address-1').val(),
+		address2 = $('#address-2').val(),
+		city = $('#city').val(),
+		state = $('#state').val(),
+		country = $('#country').val(),
+		sucursal = "7caa36207edfd028940cd642d9bddce0f3f6ab87",
+		usuario = "6ab29f84c3fd61418070b28dcf98d0130eb93d17";
 
-		var paymentData = { "jsonrpc": "2.0",
-		"method": "transaccion",
-		"params": { 
-			"data": { 
+		var paymentData = {"jsonrpc": "2.0",
+			"method": "transaccion",
+			"params": { 
+				"data": { 
 				"nombre": first,
 				"apellidos": last,
 				"numeroTarjeta": cardNumber, 
-				"cvt": cvt,
+				"cvt": cvc,
 				"cp": postalCode,
 				"mesExpiracion": month, 
 				"anyoExpiracion": year,
@@ -51,7 +53,7 @@ Template.signed.events({
 				"idSucursal": "7caa36207edfd028940cd642d9bddce0f3f6ab87",
 				"idUsuario": "6ab29f84c3fd61418070b28dcf98d0130eb93d17",
 				"idServicio": "3",
-				"email": email,
+				"email": "email@email.com",
 				"telefono": phone,
 				"celular": cell,
 				"calleyNumero": address1,
@@ -59,24 +61,35 @@ Template.signed.events({
 				"municipio": city,
 				"estado": state,
 				"pais": country, 
+				}, 
 			}, 
-		}, 
 		},
-		emptyFields = false;
+		url = "https://www.pagofacil.net/ws/public/Wsjrecurrentes/",
+		emptyFields;
 
-		$.each(paymentData, function(key, value) {
-			console.log(key, value);
-			if (value == "" ) {
-				emptyFields = true;
-				Materialize.toast('Please complete all fields.', 3000);
-			}
-		});
-/*
-    var request = $.ajax({
-			url: "https://www.pagofacil.net/st/public/Wsjtransaccion/",
-		  method: "POST",
-		  data: paymentData,
-		  dataType: "json"
-		});*/
-  },
+		function incomplete() {
+			$.each(paymentData, function(key, value) {
+				if (value === "" ) {
+					emptyFields = true;
+					console.log(key, value);
+				} else {
+					emptyFields = false;
+				}
+			});
+
+			return emptyFields;
+		}
+
+		if (incomplete()) {
+			Materialize.toast('Please complete all fields.', 3000);
+		} else {
+			Meteor.call('makePayment', url, paymentData, function(error, result) {
+				if (error) {
+					Materialize.toast(error.reason, 3000);
+				} else {
+					console.log(result);
+				}
+			});
+		}
+	},
 });
