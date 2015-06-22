ba7864021d124402934795ae1765dc3778720175
mikey <michaelrayvon@gmail.com>
2015-06-22 15:16:46 -0500

    add pagofacil payment form

--- app/client/templates/application/signed.js
+++ app/client/templates/application/signed.js
@@ -13,9 +13,50 @@ Template.signed.rendered = function () {
 };
 
 Template.signed.events({
-  'submit #payment-form': function(e){
+  'click #submit-payment': function(e){
     e.preventDefault();
 
-    $('button[type="submit"]').attr('disabled','disabled');
+    $('input#submit-payment').attr('disabled','disabled');
+
+		var paymentData = {
+			"jsonrpc": "2.0",
+			"method": "transaccion",
+			"params": {
+				"data": {
+					"nombre": "Juan",
+					"apellidos": "Lopez",
+					"numeroTarjeta": "5579567890123456", "cvt": "123",
+					"cp": "11560",
+					"mesExpiracion": "10", "anyoExpiracion": "15",
+					"monto": "100",
+					"idSucursal": "1",
+					"idUsuario": "1",
+					"idServicio": "3",
+					"email": "comprador@correo.com",
+					"telefono": "5550220910",
+					"celular": "5550123456",
+					"calleyNumero": "Anatole France 311",
+					"colonia": "Polanco",
+					"municipio": "Miguel Hidalgo",
+					"estado": "Distrito Federal",
+					"pais": "Mexico",
+					"param1": "",
+					"param2": "",
+					"param3": "",
+					"param4": "",
+					"param5": "",
+				}, 
+			},
+			"id": "test",
+		};
+
+      console.log(paymentData);
+/*
+    var request = $.ajax({
+			url: "https://www.pagofacil.net/st/public/Wsjtransaccion/",
+		  method: "POST",
+		  data: paymentData,
+		  dataType: "json"
+		});*/
   },
 });
