6514d74662c3bc8da96c95aaa508a6b0dfacce2a
Patrick Best <patrickbest@patrickbest.com>
2015-06-23 17:49:47 -0500

    split collections/applications.js to server & client

--- app/server/collections/applications.js
+++ app/server/collections/applications.js
@@ -1,15 +1,6 @@
-/* ---------------------------------------------------- +/
-
-## Applications ##
-
-All code related to the Applications collection goes here.
-
-/+ ---------------------------------------------------- */
-
 Applications = new Meteor.Collection('applications');
 
 // Allow/Deny
-// >>> this should only be declared on the server. Currently I think this is declared on the client too.
 Applications.allow({
   insert: function(userId, doc){
     return Roles.userIsInRole(userId, 'applicant');
@@ -24,24 +15,14 @@ Applications.allow({
   },
 
   remove: function(userId, doc){
-    if (Roles.userIsInRole(userId, 'applicant') && doc.userId === userId) {
-      return true;
-    } else {
-      return false;
-    }
+    return false;
   },
 });
 
 // Methods
-// >> why are these methods here?!
-// >>> should be in some kind of /methods folder && this is clients side too no?
 Meteor.methods({
   createApplication: function(application){
     Applications.insert(application);
-  },
-
-  removeApplication: function(application){
-    Applications.remove(application._id);
   },
 
   saveApplication: function(applicationId, applicationUpdate){
