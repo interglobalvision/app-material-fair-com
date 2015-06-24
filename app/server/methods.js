9602f96b319f7c9d9775a499a223df57361634b5
mikey <michaelrayvon@gmail.com>
2015-06-24 13:06:51 -0500

    set first user admin profile name to Admin

--- app/server/methods.js
+++ app/server/methods.js
@@ -5,6 +5,7 @@ Meteor.methods({
 
     if (Meteor.users.find().count() === 1) {
       Roles.addUsersToRoles(userId, ['admin',]);
+      Meteor.users.update(userId, {$set:{'profile.name':'Admin',},});
       result = 'admin';
     } else if (role === 'committee' || role === 'admin') {
       Roles.addUsersToRoles(userId, [role,]);
