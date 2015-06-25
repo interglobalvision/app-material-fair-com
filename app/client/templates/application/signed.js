535d640bc0e8e71d949a4322728a0a5b5bbb1acc
Patrick Best <patrickbest@patrickbest.com>
2015-06-25 18:29:35 -0500

    add error tracking and alert for alt payment

--- app/client/templates/application/signed.js
+++ app/client/templates/application/signed.js
@@ -2,62 +2,77 @@ Template.signed.rendered = function () {
   $(window).scrollTop( 0 );
 
   $('select').material_select();
+
+  Session.set('paymentErrors', 0);
+
+  Tracker.autorun(function() {
+    if (Session.get('paymentErrors') === 3) {
+      Materialize.toast('Try alt payment.', 3000);
+    }
+  });
 };
 
 Template.signed.events({
-	'click #submit-payment': function(e){
-		e.preventDefault();
+  'click #submit-payment': function(e){
+    e.preventDefault();
 
-		// Disable submit button
-		//$('input#submit-payment').attr('disabled','disabled');
+    // Disable submit button
+    $('#submit-payment').attr('disabled','disabled');
 
-		var paymentData = {
-			"data[nombre]": $('#first-name').val(),
-			"data[apellidos]": $('#last-name').val(),
-			"data[numeroTarjeta]": $('#card-number').val().replace(/\D/g,''),
-			"data[cvt]": $('#cvc').val().replace(/\D/g,''),
-			"data[cp]": $('#postal-code').val().replace(/\D/g,''),
-			"data[mesExpiracion]": $('#exp-month').val().replace(/\D/g,''),
-			"data[anyoExpiracion]": $('#exp-year').val().replace(/\D/g,''),
-			"data[email]": "michaelrayvon@gmail.com",
-			"data[telefono]": $('#phone').val().replace(/\D/g,''),
-			"data[celular]": $('#cell').val().replace(/\D/g,''),
-			"data[calleyNumero]": $('#address-1').val(),
-			"data[colonia]": $('#address-2').val(),
-			"data[municipio]": $('#city').val(),
-			"data[estado]": $('#state').val(),
-			"data[pais]": $('#country').val(),
-		},
-		applicationId = $('#application-id').val(),
-		emptyFields;
+    var paymentData = {
+      "data[nombre]": $('#first-name').val(),
+      "data[apellidos]": $('#last-name').val(),
+      "data[numeroTarjeta]": $('#card-number').val().replace(/\D/g,''),
+      "data[cvt]": $('#cvc').val().replace(/\D/g,''),
+      "data[cp]": $('#postal-code').val(),
+      "data[mesExpiracion]": $('#exp-month').val().replace(/\D/g,''),
+      "data[anyoExpiracion]": $('#exp-year').val().replace(/\D/g,''),
+      "data[telefono]": $('#phone').val().replace(/\D/g,''),
+      "data[celular]": $('#cell').val().replace(/\D/g,''),
+      "data[calleyNumero]": $('#address-1').val(),
+      "data[colonia]": $('#address-2').val(),
+      "data[municipio]": $('#city').val(),
+      "data[estado]": $('#state').val(),
+      "data[pais]": $('#country').val(),
+    },
+    applicationId = $('#application-id').val();
 
-		function incomplete() {
-			$.each(paymentData, function(key, value) {
-				if (value === "" ) {
-					emptyFields = true;
-					console.log(key, value);
-				} else {
-					emptyFields = false;
-				}
-			});
-			///>>> this will return false as long as the last paymentData item is not empty as the falsy is overwritten. Also does this work with the array?
+    function incomplete() {
+      var result = false;
+      var emptyFields;
 
-			return emptyFields;
-		}
+      $.each(paymentData, function(key, value) {
+        if (value === '') {
+          emptyFields = true;
+          console.log(key, value);
+        } else {
+          emptyFields = false;
+        }
 
-		if (incomplete()) {
-			Materialize.toast('Please complete all fields.', 3000);
-		} else {
-			console.log(paymentData);
-			Meteor.call('makePayment', paymentData, applicationId, function(error, result) {
-				if (error) {
-					Materialize.toast(error.reason, 3000);
-				} else if (result === 1) {
-					Materialize.toast('Thanks for your payment.', 3000);
-				} else if (result === 0) {
-					Materialize.toast('The transaction was denied.  Please make sure all fields are completed correctly.', 3000);
-				}
-			});
-		}
-	},
+        if (emptyFields) {
+          result = true;
+        }
+      });
+
+      return result;
+    }
+
+    if (incomplete()) {
+      Materialize.toast('Please complete all fields.', 3000);
+      $('#submit-payment').removeAttr('disabled');
+    } else {
+      Meteor.call('makePayment', paymentData, applicationId, function(error, result) {
+        if (error) {
+          Materialize.toast(error.reason, 3000);
+          $('#submit-payment').removeAttr('disabled');
+        } else if (result === 1) {
+          Materialize.toast('Thanks for your payment.', 3000);
+        } else if (result === 0) {
+          Materialize.toast('The transaction was denied. Please make sure all fields are completed correctly.', 3000);
+          $('#submit-payment').removeAttr('disabled');
+          Session.set('paymentErrors', (Session.get('paymentErrors') + 1));
+        }
+      });
+    }
+  },
 });
--- app/server/methods/methods.js
+++ app/server/methods/methods.js
@@ -5,22 +5,24 @@ Meteor.methods({
 
     check(applicationId, String);
 
+    params['data[email]'] = Meteor.user().emails[0].address;
     params.method = 'transaccion';
-    params['data[monto]'] = '1.00';
-    params['data[idSucursal]'] = '7caa36207edfd028940cd642d9bddce0f3f6ab87';
-    params['data[idUsuario]'] = '6ab29f84c3fd61418070b28dcf98d0130eb93d17';
-    params['data[idServicio]'] = 3;
+    params['data[monto]'] = '125.00';
     params['data[divisa]'] = 'USD';
+    params['data[idUsuario]'] = Meteor.settings.pagofacil_usuario_dev;
+    params['data[idSucursal]'] = Meteor.settings.pagofacil_sucursal_dev;
+    params['data[idServicio]'] = 3;
 
     var result,
     url = 'https://www.pagofacil.net/st/public/Wsrtransaccion/index/format/json',
     apiResult = HTTP.call('POST', url, {params: params,});
 
+//     console.log(apiResult.data);
+//     console.log(apiResult.data.WebServices_Transacciones.transaccion.error);
+
     var auth = apiResult.data.WebServices_Transacciones.transaccion.autorizado,
     transactionId = apiResult.data.WebServices_Transacciones.transaccion.transaccion,
     transaction = apiResult.data.WebServices_Transacciones.transaccion;
-
-    console.log(apiResult.data);
 
     if (auth === '1') {
       Applications.update(applicationId, {$set: {transactionId: transactionId, transaction: transaction, status: 'paid',},});
