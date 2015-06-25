fde7746173bfe335b7a1398c3799523573bf8127
mikey <michaelrayvon@gmail.com>
2015-06-24 19:40:36 -0500

    update s3 storage settings

--- app/server/collections/storage.js
+++ app/server/collections/storage.js
@@ -1,12 +1,9 @@
 var bucketName = process.env.AWS_BUCKET,
-accessKey = process.env.AWS_ACCESSKEYID,
-secretAccess = process.env.AWS_SECRETACCESSKEY,
-regionName = process.env.AWS_REGION,
 fileStore = new FS.Store.S3("storage", {
-  region: regionName, //optional in most cases
-  accessKeyId: accessKey, //required if environment variables are not set
-  secretAccessKey: secretAccess, //required if environment variables are not set
-  bucket: bucketName, //required
+  region: process.env.AWS_REGION,
+  accessKeyId: process.env.AWS_ACCESSKEYID,
+  secretAccessKey: process.env.AWS_SECRETACCESSKEY,
+  bucket: 'material-art-fair',
 });
 
 Storage = new FS.Collection("storage", {
