65244e8ad9d9b135a30756a94f56cfaf7fbffed6
mikey <michaelrayvon@gmail.com>
2015-06-29 19:30:05 -0500

    remove moved code

--- app/client/templates/application/submitted.js
+++ app/client/templates/application/submitted.js
@@ -7,26 +7,7 @@ Template.submitted.helpers({
 });
 
 Template.submitted.rendered = function () {
-  /*var canvas = document.getElementById('signature-pad');
-
-  $(window).scrollTop( 0 );
-
-  function resizeCanvas() {
-    // When zoomed out to less than 100%, for some very strange reason,
-    // some browsers report devicePixelRatio as less than 1
-    // and only part of the canvas is cleared then.
-    var ratio = Math.max(window.devicePixelRatio || 1, 1);
-
-    canvas.width = canvas.offsetWidth * ratio;
-    canvas.height = canvas.offsetHeight * ratio;
-    canvas.getContext('2d').scale(ratio, ratio);
-  }
-
-  window.onresize = resizeCanvas;
-
-  resizeCanvas();
-
-  signaturePad = new SignaturePad(canvas);*/
+  //
 };
 
 Template.submitted.events({
@@ -59,7 +40,7 @@ Template.submitted.events({
         // >>> we need error handling
         console.log(error);
       } else {
-        //$(window).scrollTop( 0 );
+        $(window).scrollTop( 0 );
       }
     });
 
