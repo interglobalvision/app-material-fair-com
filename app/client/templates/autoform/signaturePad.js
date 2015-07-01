69eca6585ad95c1aaaf6a2889a9576386921ad2f
mikey <michaelrayvon@gmail.com>
2015-07-01 13:40:34 -0500

    fix signature canvas erase on resize problem

--- app/client/templates/autoform/signaturePad.js
+++ app/client/templates/autoform/signaturePad.js
@@ -1,22 +1,35 @@
 Template.signaturePad.rendered = function () {
-  var canvas = $('canvas[name="signature"]')[0];
-
-  $(window).scrollTop( 0 );
+  var canvas = $('canvas[name="signature"]')[0],
+    ctx = canvas.getContext("2d"),
+    data = canvas.toDataURL();
 
   function resizeCanvas() {
     // When zoomed out to less than 100%, for some very strange reason,
     // some browsers report devicePixelRatio as less than 1
     // and only part of the canvas is cleared then.
-    var ratio = Math.max(window.devicePixelRatio || 1, 1);
+    var ratio = Math.max(window.devicePixelRatio || 1, 1),
+    img = new Image();
+
+    $('#signature-card').css('height', ($('#signature-card').width() * 0.4) );
 
     canvas.width = canvas.offsetWidth * ratio;
     canvas.height = canvas.offsetHeight * ratio;
-    canvas.getContext('2d').scale(ratio, ratio);
+    ctx.scale(ratio, ratio);
+
+    img.onload = function(){
+      ctx.drawImage(img,0,0,img.width,img.height,0,0,canvas.width,canvas.height);
+    };
+    
+    img.src = data;
   }
 
   window.onresize = resizeCanvas;
 
   resizeCanvas();
+
+  canvas.onmouseup = function(e){
+    data = canvas.toDataURL();
+  };
 
   signaturePad = new SignaturePad(canvas);
 };
