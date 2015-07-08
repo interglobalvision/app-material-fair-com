933f17b3ce3263795e45e36e8a10a480af296730
Patrick Best <patrickbest@patrickbest.com>
2015-07-08 13:24:55 -0500

    add new pdf download link to paid.html

--- app/client/templates/application/paid.js
+++ app/client/templates/application/paid.js
@@ -2,14 +2,8 @@ Template.paid.rendered = function () {
   $(window).scrollTop( 0 );
 };
 
+/*
 Template.paid.events({
-  'click #pdfDownload': function(e) {
-    e.preventDefault();
 
-    Blaze.saveAsPDF(Template.pdf, {
-      filename: 'material-application-2016.pdf',
-      data: Applications.findOne(),
-      format: 'letter',
-    });
-  },
 });
+*/
