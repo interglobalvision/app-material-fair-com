4e2430afaac7c06c03c1d0469728d3fab86885a2
Patrick Best <patrickbest@patrickbest.com>
2015-07-08 18:36:45 -0500

    refined archive pdf

--- app/server/routes.js
+++ app/server/routes.js
@@ -24,13 +24,13 @@
     doc.fontSize(15);
     doc.text('Material Art Fair Application', {align: 'center',});
 
-    doc.image(process.env.PWD + '/public/images/logo.png', {height: 100,});
+    doc.image(process.env.PWD + '/public/images/logo.png', 25, 25, {height: 60,});
 
-    doc.font(process.env.PWD + '/public/fonts/AkkuratPro.ttf');
-    doc.moveDown(0.5);
+    doc.moveDown(2.5);
     doc.fontSize(12);
     doc.text(TAPi18n.__('schemas.application.general.label', null, lang));
     doc.moveDown(0.5);
+    doc.font(process.env.PWD + '/public/fonts/AkkuratPro.ttf');
     doc.fontSize(11);
     doc.text(application.galleryName);
     doc.moveDown(0.5);
@@ -53,34 +53,38 @@
     doc.text(application.galleryEmail);
     doc.moveDown(0.5);
     doc.text(application.website);
-    doc.moveDown(0.5);
+    doc.moveDown(1.5);
     doc.fontSize(12);
 
     // social links
+    doc.font(process.env.PWD + '/public/fonts/Stanley-Bold.ttf');
     doc.text(TAPi18n.__('schemas.application.social.label', null, lang));
+    doc.font(process.env.PWD + '/public/fonts/AkkuratPro.ttf');
     doc.fontSize(11);
     doc.moveDown(0.5);
     if (typeof application.twitter !== 'undefined') {
-      doc.text(application.twitter);
+      doc.text('Twitter: ' + application.twitter);
     }
 
     if (typeof application.facebook !== 'undefined') {
-      doc.text(application.facebook);
+      doc.text('Facebook: ' + application.facebook);
     }
 
     if (typeof application.tumblr !== 'undefined') {
-      doc.text(application.tumblr);
+      doc.text('Tumblr: ' + application.tumblr);
     }
 
     if (typeof application.instagram !== 'undefined') {
-      doc.text(application.instagram);
+      doc.text('Instagram: ' + application.instagram);
     }
 
     doc.moveDown(1.5);
 
     // proposal
     doc.fontSize(12);
+    doc.font(process.env.PWD + '/public/fonts/Stanley-Bold.ttf');
     doc.text(TAPi18n.__('schemas.application.proposal.label', null, lang));
+    doc.font(process.env.PWD + '/public/fonts/AkkuratPro.ttf');
     doc.fontSize(11);
     doc.moveDown(0.5);
     doc.text(TAPi18n.__('schemas.application.galleryHistory.label', null, lang));
@@ -103,24 +107,14 @@
     doc.moveDown(1.5);
 
     // artists
-    doc.fontSize(12);
-    doc.text(TAPi18n.__('schemas.application.artists.label', null, lang));
-    doc.fontSize(11);
     _.each(application.artists, function(artist) {
+      doc.font(process.env.PWD + '/public/fonts/Stanley-Bold.ttf');
       doc.text(TAPi18n.__('schemas.application.artists.$.name.label', null, lang));
+      doc.moveDown(0.5);
+      doc.font(process.env.PWD + '/public/fonts/AkkuratPro.ttf');
       doc.text(artist.name);
+      doc.moveDown(0.5);
       _.each(artist.work, function(work) {
-
-        var image = HTTP.get(work.image);
-
-        var imageBuffer = new Buffer(image.content, 'binary');
-
-//         console.log(imageBuffer);
-
-        console.log(imageBuffer.toString('base64'));
-
-//         doc.image(imageBuffer.toString('base64'), {height: 100,});
-
         doc.text(work.workTitle);
         doc.text(work.medium);
         doc.text(work.dimensions);
@@ -133,30 +127,39 @@
 
     // terms
     doc.fontSize(12);
+    doc.font(process.env.PWD + '/public/fonts/Stanley-Bold.ttf');
     doc.text(TAPi18n.__('application-terms', null, lang));
-    doc.fontSize(9);
-    doc.text(TAPi18n.__('application-terms-text', null, lang));
+    doc.moveDown(0.5);
+    doc.font(process.env.PWD + '/public/fonts/AkkuratPro.ttf');
+    doc.fontSize(8);
+    doc.text(TAPi18n.__('application-terms-text-nohtml', null, lang));
     doc.moveDown(1.5);
 
     // signature
     doc.fontSize(12);
+    doc.font(process.env.PWD + '/public/fonts/Stanley-Bold.ttf');
     doc.text(TAPi18n.__('schemas.sign.signature.title', null, lang));
-    doc.moveDown(1.5);
+    doc.font(process.env.PWD + '/public/fonts/AkkuratPro.ttf');
+    doc.moveDown(0.5);
     doc.fontSize(9);
     doc.text(TAPi18n.__('application-terms-confirmation', null, lang));
+    doc.moveDown(0.5);
     doc.text(TAPi18n.__('application-terms-accept', null, lang));
     doc.moveDown(0.5);
     doc.fontSize(11);
     doc.text(application.signatureName);
     doc.text(application.signatureGallery);
+    doc.fontSize(10);
     doc.text(application.signatureDate);
     doc.moveDown(1.5);
-    doc.image(new Buffer(application.signature.replace('data:image/png;base64,','') || '', 'base64'), {height: 100,});
+    doc.image(new Buffer(application.signature.replace('data:image/png;base64,','') || '', 'base64'), {height: 70,});
     doc.moveDown(1.5);
+
+    var filename = application.galleryName.replace(/\W+/g, '') + '_Material_Art_Fair_Application_2015.pdf';
 
     this.response.writeHead(200, {
       'Content-type': 'application/pdf',
-      'Content-Disposition': 'attachment; filename=test.pdf',
+      'Content-Disposition': 'attachment; filename=' + filename,
     });
     this.response.end(doc.outputSync());
 
