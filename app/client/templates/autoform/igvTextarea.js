8b42e20606b621a2cf12ed3581293ab6953a825b
Carlos Solares <casska@gmail.com>
2015-08-05 17:26:22 -0500

    removes 'materialize-textarea' class at onCreated

--- app/client/templates/autoform/igvTextarea.js
+++ app/client/templates/autoform/igvTextarea.js
@@ -1,7 +1,3 @@
-Template.igvTextarea.onCreated(function () {
-  this.data.atts['class'] = 'materialize-textarea';
-});
-
 Template.igvTextarea.onRendered(function () {
   $('textarea').characterCounter();
 });
