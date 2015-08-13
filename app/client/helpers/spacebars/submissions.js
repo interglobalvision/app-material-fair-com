72439181e8bef8ca9a51400133af2eb27132b1a7
Patrick Best <patrickbest@patrickbest.com>
2015-08-13 14:39:57 +0100

    split /submissions display into to rate and rated

--- app/client/helpers/spacebars/submissions.js
+++ app/client/helpers/spacebars/submissions.js
@@ -1,11 +1,39 @@
+/*
 Handlebars.registerHelper('ifNoRatingShowRed', function() {
   if (this.rating) {
     return {
       class: 'card-panel',
-    }
+    };
   } else {
     return {
       class: 'card-panel red accent-2',
-    }
+    };
+  }
+});
+*/
+
+Handlebars.registerHelper('hasUserRated', function(applicationId) {
+  if (Ratings.findOne({userId: Meteor.userId(), applicationId: applicationId,})) {
+    return true;
+  } else {
+    return false;
+  }
+});
+
+Handlebars.registerHelper('hasUserNotRated', function(applicationId) {
+  if (Ratings.findOne({userId: Meteor.userId(), applicationId: applicationId,})) {
+    return false;
+  } else {
+    return true;
+  }
+});
+
+Handlebars.registerHelper('userRating', function(applicationId) {
+  var userRating = Ratings.findOne({userId: Meteor.userId(), applicationId: applicationId,});
+
+  if (userRating) {
+    return userRating.rating;
+  } else {
+    return false;
   }
 });
