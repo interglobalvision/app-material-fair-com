8d7546dab05da181deda886f7084040b70a91be2
mikey <michaelrayvon@gmail.com>
2015-09-24 11:42:50 -0500

    add exhibitor router

---
+++ app/client/router/routerExhibitor.js
@@ -0,0 +1,124 @@
+Router.map(function() {
+
+  // Exhitibor
+  this.route('exhibitorGeneral', {
+    path: '/exhibitor/general',
+    onBeforeAction: function() {
+
+    },
+
+    waitOn: function() {
+
+    },
+
+    data: function() {
+
+    },
+  });
+
+  this.route('exhibitorBadges', {
+    path: '/exhibitor/badges',
+    onBeforeAction: function() {
+
+    },
+
+    waitOn: function() {
+
+    },
+
+    data: function() {
+
+    },
+  });
+
+  this.route('exhibitorVip', {
+    path: '/exhibitor/vip',
+    onBeforeAction: function() {
+
+    },
+
+    waitOn: function() {
+
+    },
+
+    data: function() {
+
+    },
+  });
+
+  this.route('exhibitorEvents', {
+    path: '/exhibitor/events',
+    onBeforeAction: function() {
+
+    },
+
+    waitOn: function() {
+
+    },
+
+    data: function() {
+
+    },
+  });
+
+  this.route('exhibitorFloorplan', {
+    path: '/exhibitor/floorplan',
+    onBeforeAction: function() {
+
+    },
+
+    waitOn: function() {
+
+    },
+
+    data: function() {
+
+    },
+  });
+
+  this.route('exhibitorTransport', {
+    path: '/exhibitor/transport',
+    onBeforeAction: function() {
+
+    },
+
+    waitOn: function() {
+
+    },
+
+    data: function() {
+
+    },
+  });
+
+  this.route('exhibitorGuide', {
+    path: '/exhibitor/guide',
+    onBeforeAction: function() {
+
+    },
+
+    waitOn: function() {
+
+    },
+
+    data: function() {
+
+    },
+  });
+
+  this.route('exhibitorLogo', {
+    path: '/exhibitor/logo',
+    onBeforeAction: function() {
+
+    },
+
+    waitOn: function() {
+
+    },
+
+    data: function() {
+
+    },
+  });
+
+});
