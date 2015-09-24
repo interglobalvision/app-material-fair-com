9636ff30005bfb91e6283b7930ced13e1c9e1b63
mikey <michaelrayvon@gmail.com>
2015-09-24 12:25:11 -0500

    remove unused functions from exhibitor routes

--- app/client/router/routerExhibitor.js
+++ app/client/router/routerExhibitor.js
@@ -1,124 +1,40 @@
 Router.map(function() {
 
-  // Exhitibor
+  // Exhibitor
+  this.route('exhibitor', {
+    path: '/exhibitor',
+  });
+
   this.route('exhibitorGeneral', {
     path: '/exhibitor/general',
-    onBeforeAction: function() {
-
-    },
-
-    waitOn: function() {
-
-    },
-
-    data: function() {
-
-    },
   });
 
   this.route('exhibitorBadges', {
     path: '/exhibitor/badges',
-    onBeforeAction: function() {
-
-    },
-
-    waitOn: function() {
-
-    },
-
-    data: function() {
-
-    },
   });
 
   this.route('exhibitorVip', {
     path: '/exhibitor/vip',
-    onBeforeAction: function() {
-
-    },
-
-    waitOn: function() {
-
-    },
-
-    data: function() {
-
-    },
   });
 
   this.route('exhibitorEvents', {
     path: '/exhibitor/events',
-    onBeforeAction: function() {
-
-    },
-
-    waitOn: function() {
-
-    },
-
-    data: function() {
-
-    },
   });
 
   this.route('exhibitorFloorplan', {
     path: '/exhibitor/floorplan',
-    onBeforeAction: function() {
-
-    },
-
-    waitOn: function() {
-
-    },
-
-    data: function() {
-
-    },
   });
 
   this.route('exhibitorTransport', {
     path: '/exhibitor/transport',
-    onBeforeAction: function() {
-
-    },
-
-    waitOn: function() {
-
-    },
-
-    data: function() {
-
-    },
   });
 
   this.route('exhibitorGuide', {
     path: '/exhibitor/guide',
-    onBeforeAction: function() {
-
-    },
-
-    waitOn: function() {
-
-    },
-
-    data: function() {
-
-    },
   });
 
   this.route('exhibitorLogo', {
     path: '/exhibitor/logo',
-    onBeforeAction: function() {
-
-    },
-
-    waitOn: function() {
-
-    },
-
-    data: function() {
-
-    },
   });
 
 });
