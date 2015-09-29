d0b4ff701fec12151f5a0138c94a83f3ccd377ed
mikey <michaelrayvon@gmail.com>
2015-09-29 14:15:07 -0500

    add user role check to exhibitor area router

--- app/client/router/routerExhibitor.js
+++ app/client/router/routerExhibitor.js
@@ -3,38 +3,119 @@ Router.map(function() {
   // Exhibitor
   this.route('exhibitor', {
     path: '/exhibitor',
+    onBeforeAction: function() {
+      var userId = Meteor.userId();
+      
+      if (Roles.userIsInRole(userId, 'exhibitor') && moment().isAfter(Meteor.settings.public.applicationDeadline)) {
+        this.next();
+      } else {
+        Router.go('/');
+      }
+    },
   });
 
   this.route('exhibitorGeneral', {
     path: '/exhibitor/general',
+    onBeforeAction: function() {
+      var userId = Meteor.userId();
+      
+      if (Roles.userIsInRole(userId, 'exhibitor') && moment().isAfter(Meteor.settings.public.applicationDeadline)) {
+        this.next();
+      } else {
+        Router.go('/');
+      }
+    },
   });
 
   this.route('exhibitorBadges', {
     path: '/exhibitor/badges',
+    onBeforeAction: function() {
+      var userId = Meteor.userId();
+      
+      if (Roles.userIsInRole(userId, 'exhibitor') && moment().isAfter(Meteor.settings.public.applicationDeadline)) {
+        this.next();
+      } else {
+        Router.go('/');
+      }
+    },
   });
 
   this.route('exhibitorVip', {
     path: '/exhibitor/vip',
+    onBeforeAction: function() {
+      var userId = Meteor.userId();
+      
+      if (Roles.userIsInRole(userId, 'exhibitor') && moment().isAfter(Meteor.settings.public.applicationDeadline)) {
+        this.next();
+      } else {
+        Router.go('/');
+      }
+    },
   });
 
   this.route('exhibitorEvents', {
     path: '/exhibitor/events',
+    onBeforeAction: function() {
+      var userId = Meteor.userId();
+      
+      if (Roles.userIsInRole(userId, 'exhibitor') && moment().isAfter(Meteor.settings.public.applicationDeadline)) {
+        this.next();
+      } else {
+        Router.go('/');
+      }
+    },
   });
 
   this.route('exhibitorFloorplan', {
     path: '/exhibitor/floorplan',
+    onBeforeAction: function() {
+      var userId = Meteor.userId();
+      
+      if (Roles.userIsInRole(userId, 'exhibitor') && moment().isAfter(Meteor.settings.public.applicationDeadline)) {
+        this.next();
+      } else {
+        Router.go('/');
+      }
+    },
   });
 
   this.route('exhibitorTransport', {
     path: '/exhibitor/transport',
+    onBeforeAction: function() {
+      var userId = Meteor.userId();
+      
+      if (Roles.userIsInRole(userId, 'exhibitor') && moment().isAfter(Meteor.settings.public.applicationDeadline)) {
+        this.next();
+      } else {
+        Router.go('/');
+      }
+    },
   });
 
   this.route('exhibitorGuide', {
     path: '/exhibitor/guide',
+    onBeforeAction: function() {
+      var userId = Meteor.userId();
+      
+      if (Roles.userIsInRole(userId, 'exhibitor') && moment().isAfter(Meteor.settings.public.applicationDeadline)) {
+        this.next();
+      } else {
+        Router.go('/');
+      }
+    },
   });
 
   this.route('exhibitorLogo', {
     path: '/exhibitor/logo',
+    onBeforeAction: function() {
+      var userId = Meteor.userId();
+      
+      if (Roles.userIsInRole(userId, 'exhibitor') && moment().isAfter(Meteor.settings.public.applicationDeadline)) {
+        this.next();
+      } else {
+        Router.go('/');
+      }
+    },
   });
 
 });
